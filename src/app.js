082047ee60d94b4aabc8a2e686ee5057efad94f2
Shrinkhla Rajpoot <shrinkhlarajpoot.work@gmail.com>
2025-12-01 03:53:51 +0530

    routes-practice

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express =  require("express");
 const app = express();
-
-
-
-app.use("/test",(req,res)=>{
-   res.send("This is from Test")
+let arr = [{"id":1,"Fname":"Shrinkhla","Lname":"Rajpoot"},
+    {"id":2,"Fname":"Shivani","Lname":"Rajpoot"}
+]
+app.get("/test",(req,res)=>{
+   res.send("hello")
 })
-app.use("/hello",(req,res)=>{
-   res.send("This is from hello")
+app.post("/test",(req,res)=>{
+    arr.push({"id":3,"Fname":"  Pinku","Lname":"Rajpoot"})
+    res.send(" dATA ADDED TO DDATABASE SUCESSFULLY")
 })
-app.use("/",(req,res)=>{
-   res.send("This is from dashboard123")
+app.delete("/test",(req,res)=>{
+       arr.pop();
+       res.send("Deleted Succesfuully...")
 })
+
 
 app.listen(3000,()=>{
     console.log("Listening to port no 3000")
